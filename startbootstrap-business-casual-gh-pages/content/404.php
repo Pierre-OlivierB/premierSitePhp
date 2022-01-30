7c25493929a23692ab778bc60aae0f005bf53f75
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-30 12:26:55 +0100

    ajout page 404

--- startbootstrap-business-casual-gh-pages/content/404.php
+++ startbootstrap-business-casual-gh-pages/content/404.php
@@ -1,1 +1,11 @@
-No results
+<section class="page-section">
+    <div class="container">
+        <div class="quaranteDeux">
+            <h1>Page 404.</h1>
+            <div class="quatreCent">
+                <h2>Page Introuvable</h2>
+            </div>
+        </div>
+
+    </div>
+</section>
