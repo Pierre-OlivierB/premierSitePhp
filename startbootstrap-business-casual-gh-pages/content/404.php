a0702a89721cfdc314a11b8fb87f1a7756043de3
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-30 12:56:02 +0100

    amelioration page 404

--- startbootstrap-business-casual-gh-pages/content/404.php
+++ startbootstrap-business-casual-gh-pages/content/404.php
@@ -3,7 +3,7 @@
         <div class="quaranteDeux">
             <h1>Page 404.</h1>
             <div class="quatreCent">
-                <h2>Page Introuvable</h2>
+                <h2 class="not_found">Page Introuvable</h2>
             </div>
         </div>
 
